230dfb413c5dadd7631f1495de796fccebbb0136
ramesgitt <ramesh.mkrk@gmail.com>
2017-09-25 12:18:37 +0530

    second commit

--- ser.js
+++ ser.js
@@ -6,20 +6,32 @@ const port = process.env.PORT||3000
 app.set('view engine','hbs')
 hbs.registerPartials(__dirname + '/views/partials')
 
-app.use((req,res,next)=>{
-    res.render('error.hbs')
+hbs.registerHelper('setTime',()=>{
+    return new Date().getFullYear()
+    
 })
 
 
-
 app.get('/',(req,res)=>{
-    res.send("Welcome Home")
+    res.render('home.hbs')
 })
 
 app.get('/about',(req,res)=>{
 //    res.send("Git test Page")
     res.render('about.hbs')
 })
+app.get('/project',(req,res)=>{
+    res.render('project.hbs',{
+        projects:"Project Page"
+    })
+    
+})
+app.use((req,res,next)=>{
+    res.render('profile.hbs')
+    
+})
+
+
 
 app.listen(port,()=>{
     console.log(`server listen on local host 3000`)
